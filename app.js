62d95e03e56e6c2194d83a8d7da4fa9faa77b14a
lebarisunday80 <lebarisunday80@gmail.com>
2022-05-01 14:32:37 +0100

    add functionexercise

---
+++ app.js
@@ -0,0 +1,14 @@
+F = (9/5)c + 32
+function toFahrenheit(celsius){
+    let celsius = 37
+    return (9/5) * celsius +32
+    console.log(celsius);
+}
+
+function bmi(){
+    let height = 5.7
+    let weight = 60
+    let bmi =weight(height*height)
+    return bmi
+    console.log(bmi);
+}
